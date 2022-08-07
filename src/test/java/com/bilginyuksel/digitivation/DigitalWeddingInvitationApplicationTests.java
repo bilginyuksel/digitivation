9f82192520c91f00cf92c3337a472ddd5a95f28a
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-08-07 17:58:24 +0300

    feat: add project resources and tests

---
+++ src/test/java/com/bilginyuksel/digitivation/DigitalWeddingInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilginyuksel.digitivation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWeddingInvitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
