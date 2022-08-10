4bbcef9aa59736f9eeda7188be71af26ea895672
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-08-10 21:25:24 +0300

    feat!: add response entity to ports

--- src/main/java/com/bilginyuksel/digitivation/port/InvitationHttpPort.java
+++ src/main/java/com/bilginyuksel/digitivation/port/InvitationHttpPort.java
@@ -9,6 +9,7 @@ import com.bilginyuksel.digitivation.port.response.InvitationResponse;
 import com.bilginyuksel.digitivation.BusinessUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,25 +27,29 @@ public class InvitationHttpPort {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public InvitationResponse createWeddingInvitation(@RequestBody InvitationRequest request) {
-        return InvitationResponse.from(useCaseCreateInvitation.handle(request.toInvitation()));
+    public ResponseEntity<InvitationResponse> createWeddingInvitation(@RequestBody InvitationRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(InvitationResponse.from(useCaseCreateInvitation.handle(request.toInvitation())));
     }
 
     @GetMapping("/{id}")
-    public InvitationResponse getWeddingInvitation(@PathVariable String id) {
-        return InvitationResponse.from(useCaseGetInvitation.handle(id));
+    public ResponseEntity<InvitationResponse> getWeddingInvitation(@PathVariable String id) {
+        return ResponseEntity.ok(InvitationResponse.from(useCaseGetInvitation.handle(id)));
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public List<InvitationFileResponse> uploadInvitationFiles(@PathVariable String id, @RequestParam List<MultipartFile> files) throws IOException {
-        return useCaseUploadInvitation.handle(UploadInvitationFiles.create()
-                        .invitationId(id)
-                        .files(mapMultipartFiles(files))
-                        .build())
+    public ResponseEntity<List<InvitationFileResponse>> uploadInvitationFiles(@PathVariable String id, @RequestParam List<MultipartFile> files) throws IOException {
+        var uploadInvitationFiles = UploadInvitationFiles.create()
+                .invitationId(id)
+                .files(mapMultipartFiles(files))
+                .build();
+        var response = useCaseUploadInvitation.handle(uploadInvitationFiles)
                 .stream()
                 .map(InvitationFileResponse::from)
                 .toList();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
     public List<InvitationFile> mapMultipartFiles(List<MultipartFile> files) throws IOException {
