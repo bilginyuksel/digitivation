6ab043b67502eb1382118d71450d90900c06a845
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-08-13 14:52:25 +0300

    refactor! update business exception error code type

--- src/main/java/com/bilginyuksel/digitivation/BusinessException.java
+++ src/main/java/com/bilginyuksel/digitivation/BusinessException.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 
 @Getter
 public class BusinessException extends RuntimeException {
-    private final int errorCode;
+    private final String errorCode;
     private final String errorMessage;
     private Exception exception;
 
-    public BusinessException(int errorCode, String errorMessage) {
+    public BusinessException(String errorCode, String errorMessage) {
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
     }
 
-    public BusinessException(int errorCode, String errorMessage, Exception ex) {
+    public BusinessException(String errorCode, String errorMessage, Exception ex) {
         this(errorCode, errorMessage);
 
         this.exception = ex;
